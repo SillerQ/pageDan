26aeef5d3e73bca19a90de4a6f876ead8d601326
SillerQ <398568296@qq.com>
2016-05-31 22:44:49 +0800

    1.0

---
+++ js/main.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+dropMeBtn();
+// menu();
+setTimeout(headImg,200);
+});
+$(window).scroll(function () {
+	if($(window).width() > 768){
+		toggleNav();
+	}
+});
+
+// function menu(){
+// 	$("#dropdown").on("click",function(){
+// 		if($(window).width() > 768){
+// 			$("#dropMH > li:first").css("display","none");
+// 			$("#dropMH").slideDown("slow");
+// 			$("#dropMH").on("mouseover",function(){
+			
+// 		}else if($(window).width() < 768){
+// 			$("#dropMH").slideDown("slow");
+// 			$("#ulNav > li :not(:first)").hide();//隐藏除了第一个LI外的所有LI 	
+// 			$("#dropMH li:first").on("click",function(){
+// 				$("#dropMH").hide();
+// 				$("#ulNav > li").show("fast");
+// 			});
+// 		}
+// 	});
+	
+// }
+$(window).resize(function(){
+	headImg();
+})
+function headImg() {
+var res=1;
+	if($(window).width() > 420){
+		if(res==0){
+			var logoT='<a href="#" class="" id="logoTbox"><img class="logoImg logoC" src="images/tagline.svg"></a>';
+			$("#logoHead").append(logoT);
+			res=1;
+		}
+	}
+	if($(window).width() <= 420){
+		if(res==1){
+			$("#logoTbox").remove();
+			res=0;
+		}
+	}
+}
+
+
+function dropMeBtn(){
+	$("#drpBtn").on("click",function(){
+		$('#bs-example-navbar-collapse-1').slideToggle("slow");
+	});
+
+}
+
+var str=0;
+function toggleNav(){
+	if($(window).scrollTop()>250){
+		if(str===0){
+			str=1;
+			$("#nav").css({opacity:0,top:-30}).animate({opacity:1,top:0},200,function(){});
+			$("#nav").removeClass("navbar-statica");
+			$("#nav").addClass("containerHeaderS");
+			$("#nav").removeClass("containerHeader");
+			$("#logoHead").addClass("logoScroll");
+			$("#logoHead").removeClass("logo");
+			$("#bs-example-navbar-collapse-1").css({"paddingTop":"0"});
+		}
+	}
+	if($(window).scrollTop()<250){
+		if(str===1){
+			str=0;
+			$("#nav").animate({opacity:0,top:-30},200,function(){
+				$("#nav").animate({opacity:1,top:0},300);
+				$("#nav").removeClass("containerHeaderS");
+				$("#nav").addClass("containerHeader");
+				$("#logoHead").removeClass("logoScroll");
+				$("#logoHead").addClass("logo");
+			});
+
+		}
+	}
+}
