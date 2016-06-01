0a6f8d5d0a770d72cad564560385bb6695c71bd7
SillerQ <398568296@qq.com>
2016-06-01 16:41:45 +0800

    Touch

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 dropMeBtn();
 // menu();
 setTimeout(headImg,200);
+slideTouch();
 });
 $(window).scroll(function () {
 	if($(window).width() > 768){
@@ -54,6 +55,40 @@ function dropMeBtn(){
 	});
 
 }
+
+function slideTouch(){
+
+	var isTouch=('ontouchstart' in window);
+	if(isTouch){
+		$(".carousel").on('touchstart', function(e){
+			// console.log("ddd");
+			var that=$(this);
+			var touch = e.originalEvent.changedTouches[0];
+			var startX = touch.pageX;
+			var startY = touch.pageY;
+			$(document).on('touchmove',function(e){
+				touch = e.originalEvent.touches[0] ||e.originalEvent.changedTouches[0];
+				var endX=touch.pageX - startX;
+				var endY=touch.pageY - startY;
+				if(Math.abs(endY)<Math.abs(endX)){
+					if(endX > 10){
+						$(this).off('touchmove');
+						that.carousel('prev');  
+					}else if (endX < -10){
+						$(this).off('touchmove');
+						that.carousel('next');
+					}
+					return false;
+				}
+			});
+		});
+		$(document).on('touchend',function(){
+			$(this).off('touchmove');
+		});	
+	}
+}
+
+
 
 var str=0;
 function toggleNav(){
